52e0b77c7ad2a2dcfd77962e4667592a0c2d263f
Siriwat Kasamwattanarote <siriwat@live.jp>
2017-01-18 20:27:17 +0900

    Run with custom-default configurations

--- run.sh
+++ run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # Running Bitcoind
-./bitcoind/run.sh
+#./bitcoind/run.sh
 
 # Running Bitex
 docker run --rm -it \
     --net=host \
-    -v /home/stylix/codes/mxchg_docker/etc/nginx/conf.d/bitex.conf:/etc/nginx/conf.d/bitex.conf \
-    -v /home/stylix/codes/mxchg_docker/opt/bitex/config:/opt/bitex/config \
-    -v /home/stylix/codes/mxchg_docker/opt/bitex/logs:/opt/bitex/logs \
-    -v /home/stylix/codes/mxchg_docker/opt/bitex/start.sh:/opt/bitex/start.sh \
-    -v /home/stylix/codes/mxchg_docker/opt/bitex/Procfile:/opt/bitex/Procfile \
     bitex
